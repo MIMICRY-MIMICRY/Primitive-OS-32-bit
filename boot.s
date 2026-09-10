63d9f75087cd3e49a4b75b31b5cb4a6bf8805542
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-09-10 17:50:12 +0200

    Add bootloader assembly code for Multiboot

---
+++ boot.s
@@ -0,0 +1,23 @@
+[BITS 32]
+
+section .multiboot
+align 4
+    dd 0x1BADB002                   ; Magic number (Multiboot 1)
+    dd 0x00000003                   ; Flags
+    dd -(0x1BADB002 + 0x00000003)   ; Checksum
+
+section .text
+global _start
+extern kernel_main
+
+_start:
+    cli
+    mov esp, stack_top
+    call kernel_main
+    hlt
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384
+stack_top:
